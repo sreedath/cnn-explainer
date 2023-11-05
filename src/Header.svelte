7d406fbc7dde60e6788a311633c299da56560b80
Raj Dandekar <rajd@mit.edu>
2023-11-05 18:40:40 +0530

    Add student explanations

--- src/Header.svelte
+++ src/Header.svelte
@@ -58,7 +58,7 @@
 
   <div id="logo">
     <div id="logo-text">
-      CNN Explainer
+      Vizuara AI Labs
     </div>
 
 		<svg width="510px" height="50px">
@@ -75,31 +75,9 @@
 
 			<g filter="url(#chalk-texture)" transform="translate(0, 35)">
 				<text id="svg-logo-tagline">
-					Learn Convolutional Neural Network (CNN) in your browser! 
+					How do computers see images?
 				</text>
 			</g>
 		</svg>
   </div>
-
-	<div class="icons">
-	
-		<div class="icon" title="Research paper">
-			<a target="_blank" href="https://arxiv.org/abs/2004.15004">
-				<img src="PUBLIC_URL/assets/img/pdf.png" alt="pdf icon"/>
-			</a>
-		</div>
-
-		<div class="icon" title="Demo video">
-			<a target="_blank" href="https://youtu.be/HnWIHWFbuUQ">
-				<img src="PUBLIC_URL/assets/img/youtube.png" alt="youtube icon"/>
-			</a>
-		</div>
-
-		<div class="icon" title="Open-source code">
-			<a target="_blank" href="https://github.com/poloclub/cnn-explainer">
-				<img src="PUBLIC_URL/assets/img/github.png" alt="github icon"/>
-			</a>
-		</div>
-
-	</div>
 </div>
